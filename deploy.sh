2d98b621a6a2a3d9678a953d179148177d3db05a
trustytrojan <tarbot24@hotmail.com>
2024-01-12 22:02:45 -0500

    Update deploy.sh and remove nginx.conf

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-[ -e pid ] && kill $(cat pid) && rm pid
 [ ! -d .venv ] && python -m venv .venv
 source .venv/bin/activate
-pip install -r requirements.txt
-gunicorn -w 2 speedup.server:app &>log & echo $! >pid
+pip install -r requirements.txt >/dev/null
+[ ! $1 ] && (echo "Port required"; exit 1)
+gunicorn -w 2 -b 0.0.0.0:$1 speedup.server:app &>log & echo $! >pid
