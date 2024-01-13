b4ea692b638a6f6284dfc3ce70c907362b2666ee
trustytrojan <tarbot24@hotmail.com>
2024-01-12 22:19:27 -0500

    kill last server if possible

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 source .venv/bin/activate
 pip install -r requirements.txt >/dev/null
 [ ! $1 ] && (echo "Port required"; exit 1)
+kill $(cat pid) 2>/dev/null
 gunicorn -w 2 -b 0.0.0.0:$1 speedup.server:app &>log & echo $! >pid
