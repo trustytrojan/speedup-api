90ebe2b5bf11f53ec967a9771c68cba0d5a29a25
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2024-02-06 09:55:23 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ if [ ! -d .venv ]; then
 	python -m venv .venv
 	.venv/bin/pip install -r requirements.txt
 fi
-[ ! $1 ] && (echo "Port required"; exit 1)
+[ $1 ] || (echo "Port required"; exit 1)
 kill $(cat pid) 2>/dev/null
 .venv/bin/gunicorn -w 2 -b 0.0.0.0:$1 speedup.server:app &>log & echo $! >pid
