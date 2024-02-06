9b83527c3ac1dc678e9bb32380ddf8e4f0122fb3
trustytrojan <tarbot24@hotmail.com>
2024-02-06 10:14:18 -0500

    better deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
-if [ ! -d .venv ]; then
-	python -m venv .venv
-	.venv/bin/pip install -r requirements.txt
-fi
-[ $1 ] || (echo "Port required"; exit 1)
-kill $(cat pid) 2>/dev/null
+[ -d .venv ] || { python -m venv .venv; .venv/bin/pip install -r requirements.txt; }
+[ $1 ] || { echo "Port required"; exit 1; }
+kill $(<pid) 2>/dev/null
 .venv/bin/gunicorn -w 2 -b 0.0.0.0:$1 speedup.server:app &>log & echo $! >pid
