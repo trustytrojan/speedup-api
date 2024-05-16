cd0d01f21f727856d63bb97cf4ed83c7e70cfa79
trustytrojan <tarbot24@hotmail.com>
2024-05-16 14:58:33 +0000

    make deploy script `source`able

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 [ -d .venv ] || { python -m venv .venv; .venv/bin/pip install -r requirements.txt; }
-[ $1 ] || { echo "Port required"; exit 1; }
+[ $1 ] || { echo "Port required"; return 1; exit 1; }
 kill $(<pid) 2>/dev/null
 .venv/bin/gunicorn -w 2 -b 0.0.0.0:$1 speedup.server:app &>log & echo $! >pid
